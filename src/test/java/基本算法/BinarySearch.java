1c5d2f07b2742de966b1567c076b67062e09cfce
Allen <2147744928@qq.com>
2023-05-15 12:44:20 +0800

    :art:二分查找 优化、增加查找英文名

--- src/test/java/基本算法/BinarySearch.java
+++ src/test/java/基本算法/BinarySearch.java
@@ -8,9 +8,13 @@ public class BinarySearch {
      */
     @Test
     public void test1() {
-        int[] arr = {11, 26, 35, 45, 57, 68, 99, 102, 11, 26, 35, 45, 57, 68, 99, 102, 11, 26, 35, 45, 57, 68, 99, 102, 11, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 10211, 26, 35, 45, 57, 68, 99, 102, 11, 26, 35, 45, 57, 68, 99, 102, 222};
-        int target = 222;
-        binarySearch(arr, target);
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        String[] strings = {"aike","bake","ciner","dick","elk","fuck"};
+        int target = 8;
+        String target2 = "elk";
+        System.out.println(binarySearch(arr, target));
+        System.out.println("binarySearchByName(chars) = " + binarySearchByName(strings,target2));
+        Integer integer = Integer.MAX_VALUE - 1 + (Integer.MAX_VALUE - (Integer.MAX_VALUE - 1) );
     }
 
     /**
@@ -18,19 +22,45 @@ public class BinarySearch {
      * 特点：只能用于有序数组
      *
      */
-    public boolean binarySearch(int[] arr,int target){
+    public int binarySearch(int[] arr,int target){
         int left = 0;
         int right = arr.length - 1;
         while (left <= right) {
-            int median = (left + right) / 2;
+            int median = left + (right - left) / 2; // 避免极端情况整型溢出
             if (arr[median] == target) {
-                return true;
-            }
-            if (arr[median] < target) {
+                return median;
+            }else if (arr[median] < target) {
                 left = median+1;
-
+            } else if (arr[median] > target) {
+                right = median-1;
             }
         }
-        return false;
+        return -1;
+    }
+
+    /*
+    进阶：二分法查找英文名
+     */
+    public int binarySearchByName(String[] arr,String target){
+        int left = 0;
+        int right = arr.length - 1;
+
+        // 边界检查
+        if (target.compareTo(arr[left]) < 0 || target.compareTo(arr[right]) > 0) {
+            return -1;
+        }
+        while (left <= right) {
+            int median = left + (right-left)/2;
+
+            int cmp = arr[median].compareTo(target);
+            if (cmp == 0) {
+                return median;
+            } else if (cmp < 0) {
+                left = median + 1;
+            } else {
+                right = median - 1;
+            }
+        }
+        return -1;
     }
 }
