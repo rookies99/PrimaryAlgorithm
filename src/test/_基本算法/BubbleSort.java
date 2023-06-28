b9416e996a5dd09ac50d766d586d0d61286f7274
Karl <2147744928@qq.com>
2023-06-28 16:40:44 +0800

    :white_check_mark: 优化代码

--- src/test/_基本算法/BubbleSort.java
+++ src/test/_基本算法/BubbleSort.java
@@ -3,35 +3,37 @@ package _基本算法;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class BubbleSort {
 
     @Test
     public void test1() {
-        int[] arr = {4, 5, 1, 3, 2, 6, 10};
+        Random random = new Random();
+        int[] arr = new int[10000];
+        for (int i = 0; i < 10000; i++) {
+            arr[i] = random.nextInt(100000-1+1)+1;
+        }
+        long start = System.currentTimeMillis();
         System.out.println("bubbleSort(arr) = " + Arrays.toString(bubbleSort(arr)));
+        System.out.println("执行时间："+(System.currentTimeMillis()-start)+"ms");
     }
 
 
     /**
-     * 冒泡排序
+     * 冒泡排序 O(n)²
      * @param arr
      * @return
      */
     public int[] bubbleSort(int[] arr) {
-        int temp;
-        boolean flag = false;//如果传入有序数组 直接跳出循环
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j < arr.length-1; j++) {
-                if (arr[j] < arr[i]) {
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length -i -1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = temp;
-                    flag = true;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-            }
-            if (!flag) {
-                break;
             }
         }
         return arr;
