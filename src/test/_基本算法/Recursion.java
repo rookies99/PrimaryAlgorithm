4ad8e5548a2a8ec88153619400ec3dd9cada431a
Allen <2147744928@qq.com>
2023-07-06 11:31:33 +0800

    :white_check_mark: 递归获取指定文件目录下所有文件进行重命名操作

--- src/test/_基本算法/Recursion.java
+++ src/test/_基本算法/Recursion.java
@@ -2,6 +2,10 @@ package _基本算法;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.UUID;
+
 public class Recursion {
 
     /**
@@ -9,8 +13,10 @@ public class Recursion {
      */
     @Test
     public void test1() {
-        sumByRecursion(5);
+//        sumByRecursion(5);
         System.out.println("factorialRecursion(2) = " + factorialRecursion(3));
+        String directory = "D:\\Desktop\\test";
+        findDirectories(directory);
     }
 
     /**
@@ -28,6 +34,11 @@ public class Recursion {
         sumByRecursion(--i);
     }
 
+    /**
+     * 递归 求阶乘
+     * @param i
+     * @return
+     */
     public int factorialRecursion(int i) {
         if (i == 1) {
             return i;
@@ -35,4 +46,48 @@ public class Recursion {
             return i*factorialRecursion(--i);
         }
     }
+
+    /**
+     * 递归 获取指定目录下的所有文件夹
+     * @param directory
+     */
+    public void findDirectories(String directory) {
+        File rootDirectory = new File(directory);
+        if (!rootDirectory.exists() || !rootDirectory.isDirectory()) {
+            System.out.println("无效的目录路径：" + directory);
+            return;
+        }
+        traverseFiles(rootDirectory);
+    }
+
+    public void traverseFiles(File rootDirectory) {
+        File[] files = rootDirectory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println("file = " + file.getAbsolutePath());
+                    String uuid = UUID.randomUUID().toString().replace("-", "");
+                    String renamedFileName = uuid + getFileExtension(file.getName());
+                    File renamedFile = new File(file.getParent(), renamedFileName);
+                    if (file.renameTo(renamedFile)) {
+                        System.out.println("Renamed File Path: " + renamedFile.getAbsolutePath());
+                    } else {
+                        System.out.println("Failed to rename file: " + file.getAbsolutePath());
+                    }
+                } else if (file.isDirectory()){
+                    traverseFiles(file);
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取文件扩展名
+     * @param fileName
+     */
+    public String  getFileExtension(String fileName) {
+        int dotIndex = fileName.lastIndexOf(".");
+        return (dotIndex == -1) ? "" : fileName.substring(dotIndex);
+    }
+
 }
