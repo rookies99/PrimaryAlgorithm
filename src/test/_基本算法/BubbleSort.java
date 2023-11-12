a4d4dacea02df80ab2f6e3f14e6320321fa4f406
Allen <2147744928@qq.com>
2023-11-12 15:15:08 +0800

    :white_check_mark: 优化

--- src/test/_基本算法/BubbleSort.java
+++ src/test/_基本算法/BubbleSort.java
@@ -27,12 +27,12 @@ public class BubbleSort {
      */
     public int[] bubbleSort(int[] arr) {
         int temp = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length -i -1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
                 }
             }
         }
